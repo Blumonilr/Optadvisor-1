e4072f4b5c6fac1aea8b48f0919a2bfe587d7594
Cyt928 <36220406+Cyt928@users.noreply.github.com>
2018-08-11 10:22:05 +0800

    1

---
+++ app/src/main/java/utf8/optadvisor/InstantInfo.java
@@ -0,0 +1,8 @@
+package utf8.optadvisor;
+
+/**
+ * 从网上获取的即时信息
+ */
+public class InstantInfo {
+    
+}
--- app/src/main/java/utf8/optadvisor/ResetPwd.java
+++ app/src/main/java/utf8/optadvisor/ResetPwd.java
@@ -2,6 +2,8 @@ package utf8.optadvisor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * 在知道旧密码的情况下重置密码
@@ -12,5 +14,12 @@ public class ResetPwd extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reset_pwd);
+        Button button=(Button) findViewById(R.id.resetFinish);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //检查信息是否合理并发送
+            }
+        });
     }
 }
