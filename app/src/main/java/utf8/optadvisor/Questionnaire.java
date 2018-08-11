84062802580a35ebd2fc90b2862322f352a0e1bc
Cyt928 <36220406+Cyt928@users.noreply.github.com>
2018-08-11 19:11:23 +0800

    1

--- app/src/main/java/utf8/optadvisor/Questionnaire.java
+++ app/src/main/java/utf8/optadvisor/Questionnaire.java
@@ -2,8 +2,11 @@ package utf8.optadvisor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class Questionnaire extends AppCompatActivity {
+    private LinearLayout test_layout;
+
 
     @Override
     /**
@@ -13,17 +16,17 @@ public class Questionnaire extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questionnaire);
     }
-}
-
-
-
-class item{
-    private String question;
-    private String[] answers;
-    private boolean isChoosed=false;
-    private String choose;
-    public item(String question,String[] answers){
-        this.answers=answers;
-        this.question=question;
+    class item{
+        private String question;
+        private String[] answers;
+        private boolean isChoosed=false;
+        private String choose;
+        public item(String question,String[] answers){
+            this.answers=answers;
+            this.question=question;
+        }
     }
 }
+
+
+
