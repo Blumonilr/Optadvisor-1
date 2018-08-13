bffba80dae32c1dc7e37b375349b47af0f056157
Cyt928 <36220406+Cyt928@users.noreply.github.com>
2018-08-13 16:28:50 +0800

    Merge branches 'master' and 'wly' of https://github.com/Cyt928/Optadvisor
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser
    #	app/build.gradle
    #	app/src/main/res/values/colors.xml

--- app/src/main/java/utf8/optadvisor/activity/QuestionnaireActivity.java
+++ app/src/main/java/utf8/optadvisor/activity/QuestionnaireActivity.java
@@ -2,7 +2,9 @@ package utf8.optadvisor.activity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.CheckBox;
@@ -25,6 +27,14 @@ public class QuestionnaireActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questionnaire);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.tb);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
 
 
     }
