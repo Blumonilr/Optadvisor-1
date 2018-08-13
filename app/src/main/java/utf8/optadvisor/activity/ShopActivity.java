f403adc9a691702b30da3c6208614a398680379e
Blumonilr <458727969@qq.com>
2018-08-13 14:59:07 +0800

    push

---
+++ app/src/main/java/utf8/optadvisor/activity/ShopActivity.java
@@ -0,0 +1,15 @@
+package utf8.optadvisor.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import utf8.optadvisor.R;
+
+public class ShopActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop);
+    }
+}
