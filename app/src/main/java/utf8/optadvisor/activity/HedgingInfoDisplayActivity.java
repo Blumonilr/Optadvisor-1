0744dae9c937735c53d295de2be24b4157115917
Blumonilr <458727969@qq.com>
2018-08-15 11:04:29 +0800

    push

---
+++ app/src/main/java/utf8/optadvisor/activity/HedgingInfoDisplayActivity.java
@@ -0,0 +1,18 @@
+package utf8.optadvisor.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import utf8.optadvisor.R;
+
+
+public class HedgingInfoDisplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hedging_info_display);
+    }
+
+
+}
