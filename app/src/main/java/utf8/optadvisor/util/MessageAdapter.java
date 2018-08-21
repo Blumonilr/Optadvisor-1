d0d515decad3f6872945e637d8efa7bb2e264e4d
yrz <161250185@smail.nju.edu.cn>
2018-08-21 15:09:40 +0800

    后端逻辑补充1.2

--- app/src/main/java/utf8/optadvisor/util/MessageAdapter.java
+++ app/src/main/java/utf8/optadvisor/util/MessageAdapter.java
@@ -1,5 +1,6 @@
 package utf8.optadvisor.util;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -12,6 +13,7 @@ import org.w3c.dom.Text;
 import java.util.List;
 
 import utf8.optadvisor.R;
+import utf8.optadvisor.activity.MessageDetailActivity;
 import utf8.optadvisor.domain.Message;
 
 public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.ViewHolder>{
@@ -26,7 +28,17 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.ViewHold
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.item_message,parent,false);
-        ViewHolder holder=new ViewHolder(view);
+        final ViewHolder holder=new ViewHolder(view);
+        holder.messageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int postion=holder.getAdapterPosition();
+                Message message=messageList.get(postion);
+                Intent intent=new Intent(view.getContext(), MessageDetailActivity.class);
+                intent.putExtra("message",message);
+                view.getContext().startActivity(intent);
+            }
+        });
         return holder;
     }
 
@@ -35,7 +47,7 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.ViewHold
         Message message=messageList.get(position);
         //UI修改
         holder.title.setText(message.getTitle());
-        holder.content.setText(message.getContent());
+        holder.content.setText(message.getMessage());
     }
 
     @Override
@@ -45,6 +57,7 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.ViewHold
 
     static class ViewHolder extends RecyclerView.ViewHolder{
         //声明控件
+        View messageView;
         TextView title;
         TextView content;
 
@@ -52,6 +65,7 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.ViewHold
         public ViewHolder(View itemView) {
             super(itemView);
             //初始化控件
+            messageView=itemView;
             title=itemView.findViewById(R.id.title);
             content=itemView.findViewById(R.id.content);
         }
