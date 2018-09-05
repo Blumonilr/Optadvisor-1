afb55519c7b1b38feaa18356bcaf40f7dd9db60a
Cyt928 <36220406+Cyt928@users.noreply.github.com>
2018-09-05 20:33:21 +0800

    Merge branches 'master' and 'wly' of https://github.com/Cyt928/Optadvisor
    # Conflicts:
    #	app/src/main/java/utf8/optadvisor/util/NetUtil.java

--- app/src/main/java/utf8/optadvisor/util/NetUtil.java
+++ app/src/main/java/utf8/optadvisor/util/NetUtil.java
@@ -21,7 +21,7 @@ import utf8.optadvisor.domain.response.ResponseMsg;
 public enum NetUtil {
     INSTANCE;
     private Gson gson=new Gson();
-    public final static String SERVER_BASE_ADDRESS="http://172.19.120.180:8088";
+    public final static String SERVER_BASE_ADDRESS="http://10.0.142.83:8088";
     public static SharedPreferences sharedPreference;
 
     public final static int CONNECT_TIMEOUT =10000;
